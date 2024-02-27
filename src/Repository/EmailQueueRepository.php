394f82ef8b78ee7868a65b26bad5fbb40c010afc
remco van der Velde <remco.pc@outlook.com>
2024-02-27 20:19:02 +0100

    attributes in doctrine

--- src/Repository/EmailQueueRepository.php
+++ src/Repository/EmailQueueRepository.php
@@ -1,21 +1,9 @@
 <?php
 namespace Repository;
 
-use Entity\EmailQueue;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityRepository;
 
-/**
- * @method EmailQueue|null                    find($id, $lockMode = null, $lockVersion = null)
- * @method EmailQueue|null                    findOneBy(array $criteria, array $orderBy = null)
- * @method EmailQueue[]                       findAll()
- * @method EmailQueue[]                       findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
-class EmailQueueRepository extends ServiceEntityRepository
+class EmailQueueRepository extends EntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, EmailQueue::class);
-    }
 
 }
