684d0a24538f105741f324f0cfc38f86d7b32734
remco van der Velde <remco.pc@outlook.com>
2024-05-03 15:41:09 +0200

    init

--- src/Migration/email_queue.php
+++ src/Migration/email_queue.php
@@ -22,6 +22,27 @@ return function(App $object, $flags, $options) {
         //$columns = $sm->listTableColumns('user');
         ddd('exist email_queue.php');
     } else {
+        $schema = \Doctrine\DBAL\Schema\Schema();
+        $schema_table = $schema->createTable($table);
+        $schema_table->addColumn('id', 'integer', ['unsigned' => true, 'autoincrement' => true]);
+        $schema_table->addColumn('uuid', 'string', ['length' => 36]);
+        $schema_table->addColumn('to', 'text');
+        $schema_table->addColumn('reply_to', 'text', ['default' => null, 'null' => true]);
+        $schema_table->addColumn('cc', 'text', ['default' => null, 'null' => true]);
+        $schema_table->addColumn('bcc', 'text', ['default' => null, 'null' => true]);
+        $schema_table->addColumn('subject', 'text');
+        $schema_table->addColumn('text', 'text');
+        $schema_table->addColumn('body', 'text');
+        $schema_table->addColumn('attachment', 'text', ['default' => null, 'null' => true]);
+        $schema_table->addColumn('priority', 'smallint', ['default' => 1]);
+        $schema_table->addColumn('isSend', 'datetime', ['default' => null, 'null' => true]);
+        $schema_table->addColumn('isCreated', 'datetime', ['current_timestamp' => true]);
+        $schema_table->addColumn('isUpdated', 'datetime', ['current_timestamp' => true]);
+
+        $queries = $schema->toSql();
+        ddd($queries);
+
+        /*
         $introspect = $sm->introspectTable($table);
         $introspect->addColumn('id', 'integer', ['autoincrement' => true]);
         $introspect->addColumn('uuid', 'varchar', ['length' => 36]);
@@ -37,6 +58,7 @@ return function(App $object, $flags, $options) {
         $introspect->addColumn('isSend', 'datetime', ['default' => null, 'null' => true]);
         $introspect->addColumn('isCreated', 'datetime', ['current_timestamp' => true]);
         $introspect->addColumn('isUpdated', 'datetime', ['current_timestamp' => true]);
+        */
         ddd('not exist email_queue.php');
     }
 
