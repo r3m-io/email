c79979dd85102016d2eb01e61c1433c3b89bf89b
remco van der Velde <remco.pc@outlook.com>
2024-05-03 16:09:00 +0200

    init

--- src/Migration/email_queue.php
+++ src/Migration/email_queue.php
@@ -4,6 +4,7 @@ use R3m\Io\App;
 
 use R3m\Io\Module\Database;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Query\ResultSetMapping;
 
 return function(App $object, $flags, $options) {
@@ -27,20 +28,20 @@ return function(App $object, $flags, $options) {
     } else {
         $schema = new Schema();
         $schema_table = $schema->createTable($table);
-        $schema_table->addColumn('id', 'integer', ['unsigned' => true, 'autoincrement' => true]);
-        $schema_table->addColumn('uuid', 'string', ['length' => 36]);
-        $schema_table->addColumn('to', 'text');
-        $schema_table->addColumn('reply_to', 'text', ['default' => null, 'notnull' => false]);
-        $schema_table->addColumn('cc', 'text', ['default' => null, 'notnull' => false]);
-        $schema_table->addColumn('bcc', 'text', ['default' => null, 'notnull' => false]);
-        $schema_table->addColumn('subject', 'text');
-        $schema_table->addColumn('text', 'text');
-        $schema_table->addColumn('body', 'text');
-        $schema_table->addColumn('attachment', 'text', ['default' => null, 'notnull' => false]);
-        $schema_table->addColumn('priority', 'smallint', ['default' => 1]);
-        $schema_table->addColumn('isSend', 'datetime', ['default' => null, 'notnull' => false]);
-        $schema_table->addColumn('isCreated', 'datetime', ['default' => 'current_timestamp']);
-        $schema_table->addColumn('isUpdated', 'datetime', ['default' => 'current_timestamp']);
+        $schema_table->addColumn('id', Types::INTEGER, ['unsigned' => true, 'autoincrement' => true]);
+        $schema_table->addColumn('uuid', Types::STRING, ['length' => 36]);
+        $schema_table->addColumn('to', Types::TEXT);
+        $schema_table->addColumn('reply_to', Types::TEXT, ['default' => null, 'notnull' => false]);
+        $schema_table->addColumn('cc', Types::TEXT, ['default' => null, 'notnull' => false]);
+        $schema_table->addColumn('bcc', Types::TEXT, ['default' => null, 'notnull' => false]);
+        $schema_table->addColumn('subject', Types::TEXT);
+        $schema_table->addColumn('text', Types::TEXT);
+        $schema_table->addColumn('body', Types::TEXT);
+        $schema_table->addColumn('attachment', Types::TEXT, ['default' => null, 'notnull' => false]);
+        $schema_table->addColumn('priority', Types::SMALLINT, ['default' => 1]);
+        $schema_table->addColumn('isSend', Types::DATETIME_MUTABLE, ['default' => null, 'notnull' => false]);
+        $schema_table->addColumn('isCreated', Types::DATETIME_MUTABLE, ['default' => 'CURRENT_TIMESTAMP']);
+        $schema_table->addColumn('isUpdated', Types::DATETIME_MUTABLE, ['default' => 'CURRENT_TIMESTAMP']);
 
         $schema_table->setPrimaryKey(["id"]);
 
