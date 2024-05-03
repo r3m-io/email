ccd731e9ce93fbc2db02711b8f028f034c0e5026
remco van der Velde <remco.pc@outlook.com>
2024-05-03 15:27:54 +0200

    init

--- src/Migration/email_queue.php
+++ src/Migration/email_queue.php
@@ -9,16 +9,34 @@ return function(App $object, $flags, $options) {
 
     $em = Database::entityManager($object);
 
-    $schema = $em->getConnection()->createSchemaManager();
+    $sm = $em->getConnection()->createSchemaManager();
+
+    $table = 'email_queue';
 
     $tables = [
-        'email_queue'
+        $table
     ];
 
-    if ($schema->tablesExist($tables) == true){
+    if ($sm->tablesExist($tables) == true){
         // table exists! ...
+        //$columns = $sm->listTableColumns('user');
         ddd('exist email_queue.php');
     } else {
+        $introspect = $sm->introspectTable($table);
+        $introspect->addColumn('id', 'integer', ['autoincrement' => true]);
+        $introspect->addColumn('uuid', 'varchar', ['length' => 36]);
+        $introspect->addColumn('to', 'longtext');
+        $introspect->addColumn('reply_to', 'longtext', ['default' => null, 'null' => true]);
+        $introspect->addColumn('cc', 'longtext', ['default' => null, 'null' => true]);
+        $introspect->addColumn('bcc', 'longtext', ['default' => null, 'null' => true]);
+        $introspect->addColumn('subject', 'longtext');
+        $introspect->addColumn('text', 'longtext');
+        $introspect->addColumn('body', 'longtext');
+        $introspect->addColumn('attachment', 'longtext');
+        $introspect->addColumn('priority', 'smallint', ['default' => 1]);
+        $introspect->addColumn('isSend', 'datetime', ['default' => null, 'null' => true]);
+        $introspect->addColumn('isCreated', 'datetime' ['current_timestamp' => true]);
+        $introspect->addColumn('isUpdated', 'datetime' ['current_timestamp' => true]);
         ddd('not exist email_queue.php');
     }
 
