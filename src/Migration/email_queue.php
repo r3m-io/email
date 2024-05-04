2324c0022899d145dad40660e013b77784a71cfb
remco van der Velde <remco.pc@outlook.com>
2024-05-05 01:00:44 +0200

    created email_queue.json schema

--- src/Migration/email_queue.php
+++ src/Migration/email_queue.php
@@ -33,29 +33,6 @@ return function(App $object, $flags, $options) {
             'url' => $url
         ];
         $queries = Table::create($object, null, $doctrine_options);
-        /*
-        $schema = new Schema();
-        $schema_table = $schema->createTable($table);
-        $schema_table->addColumn('id', Types::INTEGER, ['unsigned' => true, 'autoincrement' => true]);
-        $schema_table->addColumn('uuid', Types::STRING, ['length' => 36]);
-        $schema_table->addColumn('to', Types::TEXT);
-        $schema_table->addColumn('reply_to', Types::TEXT, ['default' => null, 'notnull' => false]);
-        $schema_table->addColumn('cc', Types::TEXT, ['default' => null, 'notnull' => false]);
-        $schema_table->addColumn('bcc', Types::TEXT, ['default' => null, 'notnull' => false]);
-        $schema_table->addColumn('subject', Types::TEXT);
-        $schema_table->addColumn('text', Types::TEXT);
-        $schema_table->addColumn('body', Types::TEXT);
-        $schema_table->addColumn('attachment', Types::TEXT, ['default' => null, 'notnull' => false]);
-        $schema_table->addColumn('priority', Types::SMALLINT, ['default' => 1, 'length' => 2]);
-        $schema_table->addColumn('isSend', Types::DATETIME_MUTABLE, ['default' => null, 'notnull' => false]);
-        $schema_table->addColumn('isCreated', Types::DATETIME_MUTABLE, ['default' => 'CURRENT_TIMESTAMP']);
-        $schema_table->addColumn('isUpdated', Types::DATETIME_MUTABLE, ['default' => 'CURRENT_TIMESTAMP']);
-        $schema_table->setPrimaryKey(['id']);
-        $schema_table->addUniqueIndex(['uuid']);
-        $schema_table->addIndex(['priority'],'idx_priority' );
-        $schema_table->addIndex(['isSend'], 'idx_isSend');
-        $queries = $schema->toSql($platform);
-        */
         foreach($queries as $sql){
             echo $sql . PHP_EOL;
             $connection->executeStatement($sql);
