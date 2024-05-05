5251941ede50daba3a37bc3f8e5b2715e183c1b1
remco van der Velde <remco.pc@outlook.com>
2024-05-05 12:07:57 +0200

    created email_queue.json schema

--- src/Migration/email_queue.php
+++ src/Migration/email_queue.php
@@ -17,6 +17,10 @@ use R3m\Io\Doctrine\Service\Table;
 return function(App $object, $flags, $options) {
     // Your migration code here
     Database::instance($object, "system", $em, $connection, $platform, $sm);
+
+    ddd($object->config());
+
+
     $table = 'email_queue';
     Database::options($object, $connection, $sm, $options, $table, $count, $is_install);
     if($is_install === true){
